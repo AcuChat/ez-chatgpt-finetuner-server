af7f9f38e8ccb8afd31c0e2284bb88a5939fb27e
Michael Wood <michaelwood33311@icloud.com>
2024-05-08 10:59:09 -0400

    added response rows to database

--- endpoints/create.js
+++ endpoints/create.js
@@ -26,6 +26,7 @@ exports.create = async (req, res) => {
         lines = data.split("\n");
 
         for (let i = 0; i < lines.length; ++i) {
+            if (!lines[i]) continue; // ignore empty lines
             let q = `SELECT response_id FROM responses WHERE project_id = '${projectId}' AND input = ${sql.escape(lines[i])}`;
             let r = await sql.query(q);
             if (r.length) continue;
@@ -33,21 +34,19 @@ exports.create = async (req, res) => {
             const prompt = userPrompt + lines[i];
             const messages = ai.initialMessagePair(prompt, systemPrompt);
 
-            const result = await ai.getJsonResponse(openaiKey, model, messages);
-            console.log(result);
-            break;
+            const result = await ai.openAIGenericChatCompletion(openaiKey, model, messages);
+            
+            if (result.status === 'success') {
+                const responseId = uuidv4();
+                q = `INSERT INTO responses (response_id, project_id, input, orig_output, status, info) 
+                VALUES ('${responseId}', '${projectId}', ${sql.escape(lines[i])}, ${sql.escape(result.content)}, 'inserted', '{}')`;
+                r = await sql.query(q);
+            }
+            console.log(i+1, lines[i], result.content);
         }
 
     })
     
-    // cycle through inputs
-        // check if project_id / input is already in database
-            // if yes, continue to next input
-
-        // send command to model
-        
-        // add response row to database
-
     // set project status to 'created'
   
     // Respond with success message
