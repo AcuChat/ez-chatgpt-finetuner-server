c22abd2a24bd17c6bc431d2995d2073dc533a32a
Michael Wood <michaelwood33311@icloud.com>
2024-05-12 11:29:17 -0400

    added checking for bad commands for /finetune/

--- endpoints/finetune.js
+++ endpoints/finetune.js
@@ -2,7 +2,12 @@ const ai = require('../utils/ai');
 const sql = require('../utils/sql');
 
 exports.finetune = async (req, res) => {
-
+    const { projectId, numEpochs, learningRate, batchSize } = req.body;
+    if (!projectId) return res.status(400).json("Bad Command: missing projectId");
+    if (typeof numEpochs === 'undefined') return res.status(400).json('Bad Command: missing numEpochs');
+    if (typeof learningRate === 'undefined') return res.status(400).json('Bad Command: missing learningRate');
+    if (typeof batchSize === 'undefined') return res.status(400).json('Bad Command: missing batchSize');
+    
 
     return res.status(200).json('ok');
 }
