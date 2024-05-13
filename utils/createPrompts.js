dc41541cb6c420da5a0fbabe928d2a6d70ba6716
Michael Wood <michaelwood33311@icloud.com>
2024-05-13 12:22:15 -0400

    added fetching edited_output

--- utils/createPrompts.js
+++ utils/createPrompts.js
@@ -5,6 +5,12 @@ exports.createPrompts = async () => {
     const projectId = 'b3a3ac5b-8b67-4ca1-9ce6-8772dfd881fd';
     const q = `SELECT edited_output FROM responses WHERE project_id = '${projectId}'`;
     const r = await sql.query(q);
-    console.log(r);
+    
+    const prompts = [];
+    r.forEach(p => {
+        const prompt = JSON.parse(p.edited_output);
+        console.log(prompt);
+    })
+
 }
 
